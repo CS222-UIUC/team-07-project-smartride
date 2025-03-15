a92461e4cf21a433e9e710f63a08ab0c0f472cc1
Boyang Li <58096549+bobliboya@users.noreply.github.com>
2025-03-14 22:02:42 -0400

    Complete userdb create and some basic tests

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,5 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'userinfo.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
