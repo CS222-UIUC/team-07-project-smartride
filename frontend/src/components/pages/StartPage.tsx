7658d38d0038063bac029a2a8bda73dd38d8b1c3
ETwilight <hellobrian18@163.com>
2025-03-25 02:17:34 -0500

    change the default StartPage texts to current

--- frontend/src/components/pages/StartPage.tsx
+++ frontend/src/components/pages/StartPage.tsx
@@ -8,21 +8,20 @@ const StartPage: React.FC = () => {
         <div className="w-full max-w-xs flex flex-col items-center">
           <AnimLogo onFinish={() => console.log("Animation finished")} />
 
-          <h1 className="text-3xl font-bold mt-4 text-center">Vite + React</h1>
-          <h2 className="text-lg font-medium mt-2 text-center">Web Mobile Cycling App</h2>
+          <h1 className="text-xl font-bold mt-4 text-center">SmartRide</h1>
           <p className="text-sm text-gray-200 mt-3 text-center">
             Welcome to SmartRide. Click below to Login.
           </p>
 
           <div className="mt-6 w-full flex flex-col gap-3">
             <Link to="/login">
-              <button className="px-4 py-2 bg-black hover:bg-gray-800 text-white text-sm font-semibold rounded-md shadow transition w-full">
+              <button className="px-4 py-2 bg-black hover:bg-gray-800 text-black text-sm font-semibold rounded-md shadow transition w-full">
                 Go to Login
               </button>
             </Link>
 
             <Link to="/map">
-              <button className="px-4 py-2 bg-black hover:bg-gray-800 text-white text-sm font-semibold rounded-md shadow transition w-full">
+              <button className="px-4 py-2 bg-black hover:bg-gray-800 text-black text-sm font-semibold rounded-md shadow transition w-full">
                 Go to Map
               </button>
             </Link>
