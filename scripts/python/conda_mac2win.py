913096d7e127d508874a04e17df9ff440102c7f2
ETwilight <hellobrian18@163.com>
2025-03-28 03:13:05 -0500

    failure attempt to update conda scripts to automatically accomodate mac user

---
+++ scripts/python/conda_mac2win.py
@@ -0,0 +1,19 @@
+import yaml
+
+# TODO: Incorrect, for buggy content at 0x00
+
+def create_env_file_win(input_path, output_path):
+    with open(input_path, 'r') as file:
+        env_data = yaml.safe_load(file)
+    
+    env_data['dependencies'].extend(['vc=14.42', 'vs2015_runtime=14.42.34433'])
+    
+    with open(output_path, 'w') as file:
+        yaml.dump(env_data, file, default_flow_style=False)
+
+mac_file_path = 'backend/conda_env_mac.yml'
+win_file_path = 'backend/conda_env_win.yml'
+
+create_env_file_win(mac_file_path, win_file_path)
+
+print("[Backend] Environment files processed successfully, from mac to win.")
--- scripts/update-conda.ps1
+++ scripts/update-conda.ps1
@@ -4,3 +4,5 @@ Write-Host "[Backend] Exporting conda environment to conda_env_win.yml..."
 Set-Location ../backend
 conda activate smartride-backend
 conda env export --no-builds | Select-String -NotMatch "^prefix:" > conda_env_win.yml
+# Set-Location ../scripts/python
+# python conda_win2mac.py
