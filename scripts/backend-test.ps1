7acdf4e35a1bd016112a9ffc543a9f44b6a9f3e1
ETwilight <hellobrian18@163.com>
2025-03-29 01:22:48 -0500

    Place holder backend pytest with coverage

---
+++ scripts/backend-test.ps1
@@ -0,0 +1,11 @@
+# Move to backend directory
+Set-Location ../backend
+
+# Activate conda env
+conda activate smartride-backend
+
+# Run pytest with coverage
+coverage run -m pytest --cov=server tests/
+coverage report
+
+Set-Location ../scripts
