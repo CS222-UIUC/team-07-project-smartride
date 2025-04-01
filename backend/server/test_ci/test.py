df7a319f84eca6c145d9cbd9dec20624bc419309
ETwilight <hellobrian18@163.com>
2025-04-01 15:35:31 -0500

    well, if triggering twice still does not help stupid github, then github is ****

--- backend/server/test_ci/test.py
+++ backend/server/test_ci/test.py
@@ -1,12 +1,15 @@
 # test.py, useless but to test GitHub whether it can demonstrate status_checks correctly by triggering py-type-check, backend-test and format-check workflows
 
+
 def test_function() -> None:
     # This is a test function that does nothing but is written correctly
     print("This is a test function that does nothing but is written correctly.")
 
+
 # A function to demonstrate type hinting without any real use
 def add_numbers(a: int, b: int) -> int:
     return a + b
+
 
 if __name__ == "__main__":
     test_function()
