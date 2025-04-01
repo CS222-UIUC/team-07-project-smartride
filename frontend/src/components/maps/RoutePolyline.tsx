6c8b33ae8c0df609e84e4d6b4eff1c6a1adfb6cc
ETwilight <hellobrian18@163.com>
2025-04-01 00:43:55 -0500

    delete console test

--- frontend/src/components/maps/RoutePolyline.tsx
+++ frontend/src/components/maps/RoutePolyline.tsx
@@ -10,7 +10,6 @@ const defaultPoints: Array<{ lat: number; lng: number }> = [];
 const RoutePolyline: React.FC<RoutePolylineProps> = ({
   points = defaultPoints,
 }) => {
-  console.log("[RoutePolyline receive points]: ", points);
   if (points.length === 0) return null;
   const routearr: LatLngExpression[] = points.map((pt) => [pt.lat, pt.lng]);
   return (
