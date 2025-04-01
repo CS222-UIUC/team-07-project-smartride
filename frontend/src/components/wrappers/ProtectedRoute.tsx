1030f9820cdf533c8d066684f1ad1f03b9459a0a
ETwilight <hellobrian18@163.com>
2025-03-31 20:37:01 -0500

    implement @ alias

--- frontend/src/components/wrappers/ProtectedRoute.tsx
+++ frontend/src/components/wrappers/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./../context/useAuth";
+import { useAuth } from "@/components/context/useAuth";
 import { JSX } from "react";
 
 interface ProtectedRouteProps {
